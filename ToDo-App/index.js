7ee619b344433376ea6db4e7045d3e1e8cfc44f3
Rikki30 <126568482+Rikki30@users.noreply.github.com>
2023-03-16 10:55:37 +0530

    Add files via upload

---
+++ ToDo-App/index.js
@@ -0,0 +1,71 @@
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
+
+//if any pre notes are written then add it
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
+
+//for creating node
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Write your task here....";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+
+  return element;
+}
+
+//for deleting
+function deleteNote(id, element) {
+  const notes = getNotes().filter((note) => note.id != id);
+  saveNote(notes);
+  appEl.removeChild(element);
+}
+
+//for updatation
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+//adding note
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  saveNote(notes);
+}
+
+//saving in local sotarage
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+//get previous one
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
